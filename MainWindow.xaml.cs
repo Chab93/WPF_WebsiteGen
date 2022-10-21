c50e9c4e5300f06ee8edc133eb65ed4fae792154
Chab93 <vikcrille93@gmail.com>
2022-10-21 10:17:20 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Console;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string choice = "";
+        string fileName = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateWebsite_Click(object sender, RoutedEventArgs e)
+        {
+            string[] messagesToClass = { "Glöm inte att övning ger färdighet!", "Öppna boken på sida 257." };
+
+            string colorStyling;
+
+            string[] techniques = File.ReadAllLines("techniques.txt");
+
+            if (File.Exists("color-styling"))
+            {
+                colorStyling = File.ReadAllText("color-styling");
+            }
+            else
+            {
+                File.WriteAllText("color-styling", "blue");
+                colorStyling = "blue";
+            }
+
+            WebsiteGeneratorWPF.StyledWebsiteGenerator styledWebsite = new WebsiteGeneratorWPF.StyledWebsiteGenerator("Klass A", colorStyling, messagesToClass, techniques);
+
+            List<string> website1 = styledWebsite.PrintPage();
+
+            string websiteString = "";
+
+            foreach (string s in website1)
+            {
+                websiteString += s;
+            }
+
+            WebsiteOutput.Text = websiteString;
+            OutoutForDev.Text = websiteString;
+
+            choice = "codeBehind";
+        }
+
+        private void LocalHTML_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog oFD = new Microsoft.Win32.OpenFileDialog();
+            oFD.FileName = "";
+            oFD.DefaultExt = ".html";
+            oFD.Filter = "All files (*.*)|*.*";
+
+            Nullable<bool> result = oFD.ShowDialog();
+
+            if (result == true)
+            {
+                WebsiteOutput.Text = File.ReadAllText(oFD.FileName);
+                OutoutForDev.Text = File.ReadAllText(oFD.FileName);
+            }
+
+            choice = "ownFile";
+            fileName = oFD.FileName;
+        }
+
+        private void SaveForDev_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog sFD = new Microsoft.Win32.SaveFileDialog();
+            sFD.FileName = "";
+            sFD.DefaultExt = ".html";
+            sFD.Filter = "Webpage, HTML Only (.html)|*.html";
+
+            if (choice.Equals("ownFile"))
+            {
+                sFD.FileName = fileName;
+            }
+
+            Nullable<bool> result = sFD.ShowDialog();
+
+            if (result == true)
+            {
+                File.WriteAllText(sFD.FileName, OutoutForDev.Text);
+            }
+        }
+
+        private void ComboBoxColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Selection.Text = ComboBoxColor.Text;
+        }
+    }
+}
---
+++ WebsiteGeneratorWPF.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class WebsiteGeneratorWPF
+    {
+        interface Website
+        {
+            List<string> PrintPage();
+            void PrintToFile();
+        }
+        internal class WebsiteGenerator : Website
+        {
+
+            /*
+             * De olika egenskaperna (datat) i varje objekt
+             */
+            string[] messagesToClass, techniques;
+            string className;
+            string kurser = "";
+
+            /*
+             * En konstruktor som tillåter oss att lägga in egen data i objektens egenskaper
+             */
+            public WebsiteGenerator(string className, string[] messageToClass, string[] techniques)
+            {
+                this.className = className;
+                this.messagesToClass = messageToClass;
+                this.techniques = techniques;
+            }
+
+            /*
+             * Flera olika metoder för att utföra diverse funktionalitet
+             * virtual = tillåter oss att override:a (göra egen version utav) metoden i ärvda klasser
+             */
+            virtual protected string printStart()
+            {
+                return "<!DOCTYPE html>\n<html>\n<body>\n<main>\n";
+            }
+            string printWelcome(string className, string[] message)
+            {
+                string welcome = $"<h1> Välkomna {className}! </h1>";
+
+                string welcomeMessage = "";
+
+                foreach (string msg in message)
+                {
+                    welcomeMessage += $"\n<p><b> Meddelande: </b> {msg} </p>";
+                }
+
+                return welcome + welcomeMessage;
+            }
+            string printKurser()
+            {
+                return courseGenerator(this.techniques);
+            }
+            string printEnd()
+            {
+                return "</main>\n</body>\n</html>";
+            }
+
+            public List<string> PrintPage()
+            {
+                List<string> website = new();
+                website.Add(printStart());
+                website.Add(printWelcome(this.className, this.messagesToClass));
+                website.Add(printKurser());
+                website.Add(printEnd());
+
+                //Console.WriteLine(printStart());
+                //Console.WriteLine(printWelcome(this.className, this.messagesToClass));
+                //Console.WriteLine(printKurser());
+                //Console.WriteLine(printEnd());
+
+                return website;
+            }
+
+            public string getFileName()
+            {
+                string websiteName = "index";
+                Console.WriteLine("Enter filename for website: ");
+                try
+                {
+                    websiteName = Console.ReadLine();
+                }
+                catch (IOException ex)
+                {
+                    Console.Error.WriteLine(ex.Message);
+                }
+                catch (OutOfMemoryException ex)
+                {
+                    Console.Error.WriteLine(ex.Message);
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.Error.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Saving file as: " + websiteName + ".html");
+                }
+
+                return websiteName;
+            }
+
+            public void PrintToFile()
+            {
+
+                try
+                {
+                    FileInfo fi = new FileInfo(getFileName() + ".html");
+                    FileStream fs = fi.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine(printStart());
+                        sw.WriteLine(printWelcome(this.className, this.messagesToClass));
+                        sw.WriteLine(printKurser());
+                        sw.WriteLine(printEnd());
+
+                        sw.Close();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+            }
+
+            /*
+             * En utility metod
+             */
+            string courseGenerator(string[] techniques)
+            {
+
+                foreach (string technique in techniques)
+                {
+                    string tmp = technique.Trim();
+                    kurser += "<p>" + tmp[0].ToString().ToUpper() + tmp.Substring(1).ToLower() + "</p>\n";
+                }
+
+                return kurser;
+            }
+        }
+
+        /*
+     * Här ärver vi egenskaper och metoder ifrån WebsiteGenerator för att kunna återanvända delar i vår StyledWebsiteGenerator
+     */
+        internal class StyledWebsiteGenerator : WebsiteGenerator
+        {
+            // En extra egenskap
+            string color;
+
+            /*
+             * En utökad konstruktor.
+             * Vi vill lägga in alla del egenskaper som behövs i base-klassen vi ärvde ifrån
+             * Och också lägga in en färg (data) i vår nya egenskap
+             */
+            public StyledWebsiteGenerator(string className, string color, string[] messageToClass, string[] techniques) : base(className, messageToClass, techniques)
+            {
+                this.color = color;
+            }
+
+            /*
+             * Vi skapar en egen version av printStart (override:ar den) för att kunna få resultatet vi önskar
+             */
+            override protected string printStart()
+            {
+                return $"<!DOCTYPE html>\n<html>\n<head>\n<style>\np {{ color: {this.color}; }}\n" +
+                                  "</style>\n</head>\n<body>\n<main>\n";
+            }
+        }
+    }
+}
